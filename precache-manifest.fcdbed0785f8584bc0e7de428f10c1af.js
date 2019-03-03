ecb2736367dd6af50c93f107970629d33d316739
Ido <idokestin@gmail.com>
2019-03-03 13:24:12 +0200

    Initial dist subtree commit

---
+++ precache-manifest.fcdbed0785f8584bc0e7de428f10c1af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff4a6eba37fd295f74f5",
+    "url": "/<imdb-vue-proj>/css/app.d76d5657.css"
+  },
+  {
+    "revision": "ff4a6eba37fd295f74f5",
+    "url": "/<imdb-vue-proj>/js/app.73286ece.js"
+  },
+  {
+    "revision": "0f4c06d9fb888ff1fa01",
+    "url": "/<imdb-vue-proj>/js/chunk-vendors.871c9aae.js"
+  },
+  {
+    "revision": "141f66a03e7ccca21d113213246bf179",
+    "url": "/<imdb-vue-proj>/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/<imdb-vue-proj>/robots.txt"
+  }
+];
